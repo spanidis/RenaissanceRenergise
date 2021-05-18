34ae832a2f3ffbf20d26e236692fe1ae30c389c5
spanidis <p.spanidis@gmail.com>
2021-05-18 13:30:46 +0300

    first initialisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RenaissanceRenergise';
+  title = 'RenaissanceRenergise_v1';
 }
+~
